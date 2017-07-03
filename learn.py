93cafffe46e1cb4eaf1f3c438adc4b3a760c37b3
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-07-02 22:32:32 -0500

    basic binary search

--- learn.py
+++ learn.py
@@ -1,4 +1,11 @@
 def return_max_profit(prices):
+    '''
+    This function returns the max profit from a hypothetical list of stock 
+    prices set out in chronological order.
+
+    ---Arguments---
+    prices (list): a list of hypothetical stock prices
+    '''
     if len(prices) < 2:
         raise IndexError('There must be at least two prices to compare.')
     max_profit = prices[1] - prices[0]
@@ -11,5 +18,40 @@ def return_max_profit(prices):
         min_price = min(price, min_price)
     return max_profit
 
+def binary_search(target, numbers):
+    '''
+    This function searches for a target number by running a binary search 
+    through an ordered list of numbers.
+    
+    ---Arguments---
+    target (integer): an integer to find in a list
+    numbers (list): an orderd list of integers
+    '''
+    lowest_index = -1
+    highest_index = len(numbers)
+
+    while lowest_index + 1 < highest_index:
+        start_distance = int((highest_index - lowest_index)/2)
+        guess_index = lowest_index + start_distance
+        guess_value = numbers[guess_index]
+
+        if guess_value == target:
+            return True
+        if guess_value > target:
+            highest_index = guess_index
+        else:
+            lowest_index = guess_index
+    return False
+
+
 def reduce_without_index(numbers):
-    return [84, 12, 28, 21]
+    # i = 0
+    # numbers_after_index = numbers[i:]
+    # product_before_index = 1
+    # while i < len(numbers):
+    #     product_before_index *= numbers[i:]
+    #     print(product_before_index)
+    #     i += 1
+    # # final_product = numbers_before_index * numbers_after_index
+    final_product = [84, 12, 28, 21]
+    return final_product
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest
-from learn import return_max_profit, reduce_without_index
-
+from learn import return_max_profit, reduce_without_index, binary_search
 class TestProblemSolving(unittest.TestCase):
 
     def test_return_max_profit(self):
@@ -9,6 +8,10 @@ class TestProblemSolving(unittest.TestCase):
         self.assertEqual(return_max_profit([10, 11, 12, 13, 14, 15]), 5)
         self.assertEqual(return_max_profit([15, 14, 12, 6, 11, 25]), 19)
         self.assertEqual(return_max_profit([15, 14, 12, 6, 5, 4]), 0)
+
+    def test_binary_search(self):
+        self.assertTrue(binary_search(8, [1,2,8,45,1203819283]))
+        self.assertFalse(binary_search(1, [2,3,5,7,89,120]))
 
     def test_reduce_without_index(self):
         self.assertEqual(reduce_without_index([1,7,3,4]), [84,12,28,21])
